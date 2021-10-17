c5b8c54e7e002ba8e08854ee3f9b2004871d6ec6
kcc7907 <kcc7907@gmail.com>
2021-10-17 16:11:41 +0800

    fix: reder & image optimize

--- js/header.js
+++ js/header.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(function () {
   // Navbar_Mobile
   let nav = $(".navMobile");
   let ul = $("nav ul");
@@ -40,64 +40,5 @@ $(document).ready(function () {
       nav.removeClass("-close");
     }
   });
-
-  function contactNone() {
-    $("#contact").hide();
-    $("html").css({ overflow: "auto" });
-    $("#contact form").addClass('animate')
-  }
-
-  function contactShow() {
-    $("#contact").css({ display: "flex" });
-    setTimeout(() => {
-        $("#contact form").removeClass('animate')
-    }, 100);
-    $("html").css({ overflow: "hidden" });
-  }
-
-  $(".contactUs").on("click", contactShow);
-  $(".contactUs_nav").on("click", contactShow);
-  $("#applicationButton").on("click", contactShow);
-  $("#contact .-close").on("click", contactNone);
-  $("#contact .bg").on("click", contactNone);
-
-  $("#contact button").on("click", () => {
-    const errors = [];
-    const values = [...$("#contact .validate")];
-    values.forEach((e) => {
-      if (e.value === "") errors.push(e);
-    });
-    if (errors.length !== 0) {
-      errors.forEach((e) => {
-        $(e).addClass("-error");
-      });
-      return;
-    }
-    sendMail();
-  });
-
-  function sendMail() {
-    if (!emailjs) {
-      alert("Something error!!");
-      return;
-    }
-    const sid = atob("c2VydmljZV8xd292NzNr");
-    const uid = atob("dXNlcl9BeU5DbWpnMlVzWTIwdkhDUzlKdVI=");
-    data = {
-      from_name: $("#contact #name").val(),
-      reply_to: $("#contact #email").val() || "沒留信箱",
-      message: $("#contact #message").val(),
-      to_name: "kou",
-    };
-    emailjs.send(sid, "template_xq95tgt", data, uid).then(
-      function () {
-        contactNone();
-      },
-      function (error) {
-        alert("Email server error");
-        console.error("Email server error", error);
-      }
-    );
-  }
 
 });
--- js/index.js
+++ js/index.js
@@ -1,8 +1,4 @@
 $(function () {
-  $(".headerPage").load("header.html");
-});
-
-$(document).ready(function () {
   $(".footerPage").load("footer.html");
   // Navbar_Mobile
   let nav = $(".navMobile");
@@ -45,4 +41,63 @@ $(document).ready(function () {
       nav.removeClass("-close");
     }
   });
+
+  function contactNone() {
+    $("#contact").hide();
+    $("html").css({ overflow: "auto" });
+    $("#contact form").addClass('animate')
+  }
+
+  function contactShow() {
+    $("#contact").css({ display: "flex" });
+    setTimeout(() => {
+        $("#contact form").removeClass('animate')
+    }, 100);
+    $("html").css({ overflow: "hidden" });
+  }
+
+  $(".contactUs").on("click", contactShow);
+  $(".contactUs_nav").on("click", contactShow);
+  $("#applicationButton").on("click", contactShow);
+  $("#contact .-close").on("click", contactNone);
+  $("#contact .bg").on("click", contactNone);
+
+  $("#contact button").on("click", () => {
+    const errors = [];
+    const values = [...$("#contact .validate")];
+    values.forEach((e) => {
+      if (e.value === "") errors.push(e);
+    });
+    if (errors.length !== 0) {
+      errors.forEach((e) => {
+        $(e).addClass("-error");
+      });
+      return;
+    }
+    sendMail();
+  });
+
+  function sendMail() {
+    if (!emailjs) {
+      alert("Something error!!");
+      return;
+    }
+    const sid = atob("c2VydmljZV8xd292NzNr");
+    const uid = atob("dXNlcl9BeU5DbWpnMlVzWTIwdkhDUzlKdVI=");
+    data = {
+      from_name: $("#contact #name").val(),
+      reply_to: $("#contact #email").val() || "沒留信箱",
+      message: $("#contact #message").val(),
+      to_name: "kou",
+    };
+    emailjs.send(sid, "template_xq95tgt", data, uid).then(
+      function () {
+        contactNone();
+      },
+      function (error) {
+        alert("Email server error");
+        console.error("Email server error", error);
+      }
+    );
+  }
 });
